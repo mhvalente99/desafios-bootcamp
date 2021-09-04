b48ef8df3989eeef600d338f0d064ec4b8371e75
Matheus <matheushenrique4@hotmail.com>
2021-09-04 15:22:48 -0300

    #2 - Criando os componentes diretamente no app

--- desafio-1/src/App.js
+++ desafio-1/src/App.js
@@ -1,6 +1,12 @@
 function App() {
     return (
-        <h1>App</h1>
+        <>
+            <header className="header">Header</header>
+            <nav className="menu">Menu</nav>
+            <aside className="sidebar">Sidebar</aside>
+            <main className="content">Content</main>
+            <footer className="footer">Footer</footer>
+        </>
     )
 }
 
