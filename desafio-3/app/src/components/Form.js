465d0abc6e51e448092ac6374a08ad51ab9d852e
Matheus <matheushenrique4@hotmail.com>
2021-09-09 23:06:41 -0300

    #5 - Cadastrando carro e listando os carros ja cadastrados na api

--- desafio-3/app/src/components/Form.js
+++ desafio-3/app/src/components/Form.js
@@ -1,6 +1,38 @@
 function Form() {
+    const url = "http://localhost:3333/cars"
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        
+        const dadosCarro = {
+            image: e.target.elements.image.value,
+            brandModel: e.target.elements.model.value,
+            year: e.target.elements.year.value,
+            plate: e.target.elements.licensePlate.value,
+            color: e.target.elements.color.value
+        }
+        console.log(dadosCarro);
+
+        cadastrarCarro(dadosCarro)
+
+    }
+
+    async function cadastrarCarro(dados) {
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(dados)
+        })
+        .then(
+          console.log("buscar novos carros")
+        )
+        .catch(err => {console.log(err);})
+      }
+
     return(
-        <form name="form-car">
+        <form name="form-car" onSubmit={handleSubmit}>
             <p>Cadastro de Carro</p>
             <label htmlFor="image">URL Imagem</label>
             <input type="url" name="image" id="image"/>
--- desafio-3/app/src/components/Table.js
+++ desafio-3/app/src/components/Table.js
@@ -1,4 +1,43 @@
+import { useState, useEffect } from 'react'
+
 function Table() {
+    const url = "http://localhost:3333/cars"
+    const [cars, setCars] = useState([]);
+
+    async function listarCarros() {
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            setCars(data)
+          })
+    }
+
+    useEffect(() => {
+        listarCarros()
+    }, [])
+
+    function montarListaCarroVazio() {
+        return (
+            <tr>
+                <td>Nenhum carro encontrado</td>
+            </tr>
+        )
+    }
+
+    function montarListaCarro(cars) {
+        console.log(cars);
+        return cars.map((car) => (
+                <tr>
+                    <td>{car.image}</td>
+                    <td>{car.brandModel}</td>
+                    <td>{car.year}</td>
+                    <td>{car.plate}</td>
+                    <td>{car.color}</td>
+                </tr>
+        )
+     )
+    }
+
     return(
         <table data-js="table" border="1">
             <thead>
@@ -10,6 +49,11 @@ function Table() {
                     <th>Cor</th>
                 </tr>
              </thead>
+             <tbody>
+                 {
+                     cars.length > 0 ? montarListaCarro(cars) : montarListaCarroVazio()
+                 }                
+             </tbody>
         </table>
     )
 }
